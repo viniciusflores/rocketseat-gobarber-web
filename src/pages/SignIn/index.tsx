11efc362a0d0781a28069c24fffdf9eed0099199
vinicius <viniciusflores379@gmail.com>
2020-05-30 17:47:58 +0000

    :sparkles: Create hook useAuth()

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useRef, useContext } from 'react'
+import React, { useCallback, useRef } from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
 
-import { AuthContext } from '../../context/AuthContext'
+import { useAuth } from '../../context/AuthContext'
 import getValidationErrors from '../../util/getValidationsErrors'
 
 import logoImg from '../../assets/logo.svg'
@@ -21,8 +21,8 @@ interface SignInFormData {
 
 const SingIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
-  const { user, signIn } = useContext(AuthContext)
-
+  const { user, signIn } = useAuth()
+  console.log(user)
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
       try {
