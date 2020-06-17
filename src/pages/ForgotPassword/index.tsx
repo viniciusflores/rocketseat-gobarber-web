72302064fe9ab561dc4edc7022273f9cb39d0f31
Vinicius Flores <viniciusflores379@gmail.com>
2020-06-17 20:45:15 +0000

    :bug: fix route signin

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -89,7 +89,7 @@ const ForgotPassword: React.FC = () => {
             </Button>
           </Form>
 
-          <Link to="/signin">
+          <Link to="/">
             <FiLogIn />
             Voltar ao login
           </Link>
