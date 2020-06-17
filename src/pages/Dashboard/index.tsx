ad9379743e12879ff5a5c8c52d61ccb596e1b1ce
Vinicius Flores <viniciusflores379@gmail.com>
2020-06-17 21:15:53 +0000

    :sparkles: create dashboard header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import { Container } from './styles'
+import { FiPower } from 'react-icons/fi'
+import { Container, Header, HeaderContent, Profile } from './styles'
+import logoImg from '../../assets/logo.svg'
+import { useAuth } from '../../hooks/auth'
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth()
+
   return (
     <Container>
-      <h1>DASHBOARD</h1>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem Vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
     </Container>
   )
 }
