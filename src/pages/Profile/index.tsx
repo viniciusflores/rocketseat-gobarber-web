90749fdcc6f93342b4918ca09a2c5dbe775cb958
Vinicius Flores <viniciusflores379@gmail.com>
2020-06-29 21:36:38 +0000

    :art: Add favicon

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -77,7 +77,7 @@ const Profile: React.FC = () => {
             : {}),
         }
 
-        const response = await api.put('/profile', data)
+        const response = await api.put('/profile', formData)
 
         updateUser(response.data)
 
